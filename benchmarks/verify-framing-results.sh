63f0140dc678e0ff9d6c19a6b74f3e9494b55dd0
Alice Williams <alicewilliams438@hotmail.com>
2026-09-03 00:14:04 +0100

    M51 record stream framing baseline

--- benchmarks/verify-framing-results.sh
+++ benchmarks/verify-framing-results.sh
@@ -24,7 +24,9 @@ awk -F '\t' -v expected="$expected_results" -v cases="$expected_cases" '
   NR == 1 { if ($0 != "implementation\tcase\tsegments\tpasses\trun\telapsed_ns\tchecksum") exit 1; next }
   NF != 7 || $4 != 50000 || $6 <= 0 || $7 < 0 { exit 1 }
   $1 != "cpp" && $1 != "native" { exit 1 }
-  $2 != "parse" && $2 != "encode" && $2 != "encode-prepared" { exit 1 }
+  $2 != "parse" && $2 != "encode" && $2 != "encode-prepared" &&
+      $2 != "stream-read" && $2 != "stream-write" &&
+      $2 != "stream-write-prepared" { exit 1 }
   $3 != 1 && $3 != 2 && $3 != 64 { exit 1 }
   !($2 FS $3 in checksum) { checksum[$2 FS $3] = $7; next }
   $7 != checksum[$2 FS $3] { exit 1 }
